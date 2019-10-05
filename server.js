b236188c6af526596027f158fd8819a9fe97f906
Johanna Maarja Eller <43164763+jmaarjae@users.noreply.github.com>
2019-10-05 13:52:09 +0300

    Lab 1: Server init

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("Rakenduste-proge-eller", "static", "index.html"));
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
