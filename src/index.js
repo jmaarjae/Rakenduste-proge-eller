172aca1e6f65f93e5d157e7fa839300572d85168
Johanna Maarja Eller <43164763+jmaarjae@users.noreply.github.com>
2019-10-14 00:54:37 +0300

    Lab3: React hello worl

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-//const homepage = require("./homepage.js");
-//const itempage = require("./itempage.js");
+import React from "react";
+import ReactDOM from "react-dom";
 
-import homepage from "./homepage.js";
-import itempage from "./itempage.js";
+const root = document.getElementById("app");
 
-//console.log("index for webpack");
-
-window.addEventListener("load", () => {
-    homepage.setup();
-    itempage.setup();
-   });
+ReactDOM.render(
+    React.createElement(
+        "button",
+        {},
+        "I am a button, hello world!"
+    ),
+    root
+);
