29abffdaab19e93adc1b88f9c19743330882bb11
Maarja <johannaeller@gmail.com>
2019-11-15 18:23:04 +0200

    Lab5: checkbox design but functionality is'nt working properly

--- src/Checkbox.jsx
+++ src/Checkbox.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./checkbox.css"; //https://codepen.io/mburnette/pen/LxNxNg/
 
 //HomePageist name ja onChange
-const Checkbox = ({ name, onChange, checked}) => (
+const Checkbox = ({ name, onChange }) => (
   <div>
-    <label>
-      {name}
-      <input
-        name={name}
-        type="checkbox"
-        checked={checked}
-        onChange={onChange}
-      />
-    </label>
+    <div className="togglerName">{name}</div>
+    <input id={name} type="checkbox" onChange={onChange} />
+    <label htmlFor={name}></label>
   </div>
 );
 
 Checkbox.propTypes = {
-    name: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired,
-    checked: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  checked: PropTypes.bool.isRequired
 };
 
 export default Checkbox;
